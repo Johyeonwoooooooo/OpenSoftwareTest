0208664bf7cec9b36e1dd90aeecd5ee15930464e
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-12 18:32:44 +0900

    test

--- ingredient_based/model_pickle_test.py
+++ ingredient_based/model_pickle_test.py
@@ -2,8 +2,8 @@ import numpy as np
 import re
 import pickle
 
-file_path = "C:\\OpenSoftware_test\\recipe_ingredients.txt" # labtop
-
+# file_path = "C:\\OpenSoftware_test\\recipe_ingredients.txt" # labtop
+file_path = "C:\\OpenSoftware_test\\recipe_ingredients.txt"
 recipes = []
 
 with open(file_path, 'r', encoding='utf-8') as file:
@@ -18,10 +18,12 @@ with open(file_path, 'r', encoding='utf-8') as file:
 with open('ingredient_based_recommend_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
-similarities = loaded_model.score(['돼지고기', '김치'])
+similarities = loaded_model.score(['돼지고기', '설탕'])
 
 
 most_similar_indices = np.argsort(similarities)[::-1][:5]
 print("Most similar recipes (top 5):")
 for index in most_similar_indices:
     print(recipes[index])
+
+print(most_similar_indices)
